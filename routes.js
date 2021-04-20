f6f41830915e7571b49fa122d38b34e6adf863b0
miroslavuzunov <71848353+miroslavuzunov@users.noreply.github.com>
2021-04-20 16:40:17 +0300

    register page

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const router = Router() 
 
 
+
 router.get('/',(req,res) =>{
     res.render('home', {title: 'CarsExpress'})
 })
@@ -11,6 +12,8 @@ router.get('/add',(req,res) =>{
 router.get('/details/:carId',(req,res) =>{
     res.render('details', {title: 'CarsExpress'})
 })
-
+router.get('/register',(req,res) =>{
+    res.render('register', {title: 'CarsExpress'})
+})
 
 module.exports = router
