2237b5b8fb65c08966542b0cbd9630f4f66e0836
miroslavuzunov <71848353+miroslavuzunov@users.noreply.github.com>
2021-05-10 20:33:02 +0300

    initial card design

--- services/addService.js
+++ services/addService.js
@@ -1,22 +1,22 @@
 const Car = require('../model/car')
-const Brands =require('../model/brands')
+const Brands = require('../model/brands')
 
+async function create(brandName, carData, userId) {
 
-async function create(brandName,carData, userId) {
+    let brandId = await Brands.findOne({ brand: brandName }).lean()
 
-    let brandId = await Brands.findOne({brand:brandName}).lean()
-
-    let car = new Car({brand: brandId._id ,model:carData.model,price: carData.price,imageUrl: carData.imageUrl, owner: userId })
+    let car = new Car({ brand: brandId._id, model: carData.model, price: carData.price, imageUrl: carData.imageUrl, owner: userId })
 
     return car.save()
 }
-async function getBrands(){
+
+async function getBrands() {
     let brands = await Brands.find({}).lean()
 
     return brands
 }
 
-function createBrand(brand){
+function createBrand(brand) {
     let brandName = new Brands(brand)
 
     return brandName.save()
