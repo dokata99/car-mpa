a8a2bd6947604ff7ee568635b265671c49b3c8ac
miroslavuzunov <71848353+miroslavuzunov@users.noreply.github.com>
2021-05-15 20:41:13 +0300

    models bug fixed

--- public/scripts/fillModels.js
+++ public/scripts/fillModels.js
@@ -1,7 +1,7 @@
 const dropdown = document.getElementById("brand");
 const searchBrandDropdown = document.getElementById("searchBrand")
 
-if (dropdown != null) {
+if (dropdown !== null) {
     dropdown.addEventListener("change", function() {
         let selectedBrand = dropdown.options[dropdown.selectedIndex].text;
 
@@ -26,42 +26,44 @@ if (dropdown != null) {
     })
 }
 
-if (searchBrandDropdown !== "all") {
+if (searchBrandDropdown !== null) {
     searchBrandDropdown.addEventListener("change", function() {
-        let selectedBrand = searchBrandDropdown.options[searchBrandDropdown.selectedIndex].text;
+        if (searchBrandDropdown.value !== "all") {
+            let selectedBrand = searchBrandDropdown.options[searchBrandDropdown.selectedIndex].text;
 
-        fetch('/add/' + selectedBrand)
-            .then(res => {
-                return res.text()
-            })
-            .then(data => {
-                let models = JSON.parse(data);
-
-                let divModel = document.getElementById("searchModel")
-                divModel.innerHTML = ""
-
-                models.forEach(function(model) {
-                    let checkbox = document.createElement("input");
-                    checkbox.type = "checkbox";
-                    checkbox.id = model._id;
-                    checkbox.name = "searchModel";
-                    checkbox.value = model.model;
-                    checkbox.innerHTML = model.model;
-
-                    let label = document.createElement("label");
-                    label.htmlFor = model._id;
-                    label.appendChild(document.createTextNode(model.model));
-
-                    let br = document.createElement("br");
-
-                    divModel.appendChild(checkbox);
-                    divModel.appendChild(label);
-                    divModel.appendChild(br);
+            fetch('/add/' + selectedBrand)
+                .then(res => {
+                    return res.text()
                 })
-            })
-            .catch(error => console.log(error))
+                .then(data => {
+                    let models = JSON.parse(data);
+
+                    let divModel = document.getElementById("searchModel")
+                    divModel.innerHTML = ""
+
+                    models.forEach(function(model) {
+                        let checkbox = document.createElement("input");
+                        checkbox.type = "checkbox";
+                        checkbox.id = model._id;
+                        checkbox.name = "searchModel";
+                        checkbox.value = model.model;
+                        checkbox.innerHTML = model.model;
+
+                        let label = document.createElement("label");
+                        label.htmlFor = model._id;
+                        label.appendChild(document.createTextNode(model.model));
+
+                        let br = document.createElement("br");
+
+                        divModel.appendChild(checkbox);
+                        divModel.appendChild(label);
+                        divModel.appendChild(br);
+                    })
+                })
+                .catch(error => console.log(error))
+        } else {
+            let divModel = document.getElementById("searchModel")
+            divModel.innerHTML = ""
+        }
     })
-} else {
-    let divModel = document.getElementById("searchModel")
-    divModel.innerHTML = ""
 }
